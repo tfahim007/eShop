a3e2159ada9eb8afd4ae5ff59541ff70470fcb0d
Tahmidul <tfahim2016@gmail.com>
2022-04-02 13:37:53 +0600

    Chat integrated

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -50,8 +50,10 @@
     
     <div class="content">
       @yield('content')
-      
     </div>
+
+    
+    
 
   </main>
 <!--   Core JS Files   -->
@@ -62,6 +64,20 @@
   <script src="{{ asset('frontend/js/owl.carousel.min.js') }}"></script>
   <script src="{{ asset('frontend/js/custom.js') }}"></script>
   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+  <!--Start of Tawk.to Script-->
+  <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/6247e36f2abe5b455fc35374/1fvkd0ra8';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
 
   @if((session('status'))){
     <script>
